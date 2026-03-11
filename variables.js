785590536a6137cf08fd780e0d31f2b69b618d86
Aryansh Kurmi <arukurmi22@gmail.com>
2026-03-11 14:24:29 +0530

    update variables

---
+++ variables.js
@@ -0,0 +1,5 @@
+https://jobs.careers.microsoft.com/global/en/share/1731796/?utm_source=Job Share&utm_campaign=Copy-job-share
+https://jobs.careers.microsoft.com/global/en/job/1682952/Software-Engineer
+https://jobs.careers.microsoft.com/global/en/job/1738001/Software-Engineer
+https://jobs.careers.microsoft.com/global/en/job/1744843/Software-Engineer---Azure-Files
+https://jobs.careers.microsoft.com/global/en/job/1738488/Software-Engineer-II
